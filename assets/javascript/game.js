f72465801ebeae53620651316b394568ca88fb46
Ed Courtney <edcourtney74@gmail.com>
2018-11-08 20:25:09 -0500

    Logic improved and game is functional, but commas need removed from array. Needs CSS styling.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,9 +4,9 @@
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 // Array for possible words to guess
-var puzzleWords = ["cardinals", "catcher", "pitcher", "incompletion", "lateral", "downtown", "mizzou", "dugout", "sabermetrics", "icing", "volley",
-    "hurdles", "badminton", "wicket", "equestrian", "freestyle", "javelin", "knuckleball", "goalkeeper", "recruiting",
-    "nascar", "offsides", "swish", "traveling", "softball"]
+var puzzleWords = ["cardinals", "catcher", "pitcher", "incompletion", "lateral", "downtown", "stadium", "dugout", "sabermetrics", "icing", "bobsled",
+    "hurdles", "badminton", "wicket", "equestrian", "backstroke", "javelin", "slider", "goaltender", "recruiting",
+    "nascar", "tailgating", "swish", "traveling", "decathlon"]
 
 // Array for letters guessed so far, which is blank at the start of the game
 var lettersGuessed = [];
@@ -14,14 +14,24 @@ var lettersGuessed = [];
 // Variable that contains Wins
 var wins = 0;
 
-// Variable that contains Guesses Left
+// Variable that contains the number of Guesses Left
 var guessesLeft;
 
-// Variable that contains random puzzle selected
+// Variable that contains the random puzzle selected
 var currentPuzzle;
 
-// Array that contains the individual letters of the current puzzle
+// Array that is a master that contains the individual letters of the current puzzle - this will not be manipulated
+// This is primarily for use to fill in the blanks on the display
+var masterPuzzleLetters = [];
+
+// Same array as masterPuzzleLetters above but will be manipulated
 var puzzleLetters = [];
+
+// Array that substitutes letters for blanks for display
+var blanks = [];
+
+// Array that will hold display letters
+var displayLetters = [];
 
 // Variable that contains letter chosen by user
 var userLetter;
@@ -40,21 +50,28 @@ function startGame() {
     currentPuzzle = puzzleWords[Math.floor(Math.random() * puzzleWords.length)];
     // Convert currentPuzzle to an array of a string of letters
     puzzleLetters = currentPuzzle.split('');
-    console.log(`Array of letters in the puzzle: ${puzzleLetters}`);guessesLeft = 7;
-   
+    // Convert currentPuzzle to an array of a string of letters - master copy to compare guesses to
+    masterPuzzleLetters = currentPuzzle.split('');
+    // Created same array as puzzleLetters, but to be converted to blanks
+    displayLetters = currentPuzzle.split('');
+
+    // Convert display letters into blanks (underscores)
+    for (let i = 0; i < displayLetters.length; i++) {
+        displayLetters.splice(i, 1, "_");
+    }
+
+    // Display puzzle blanks on screen
+    puzzleText.innerHTML = displayLetters;
+
     // displays guesses left on screen
     guessesLeft = 7;
     guessesLeftText.textContent = guessesLeft;
-    
+
     // empties lettersGuessed array, displays on screen
     lettersGuessed = [];
     lettersGuessedText.textContent = lettersGuessed;
-    
-    
-    instructionsText.textContent = "Press a key to begin!";
-    console.log("Game started");
-    console.log(`Current puzzle selected: ${currentPuzzle}.`);
 
+    instructionsText.textContent = "Press a key to begin!";
 }
 
 // MAIN PROCESS===============================================
@@ -82,52 +99,60 @@ document.onkeyup = function (event) {
             lettersGuessed.push(userLetter);
             lettersGuessedText.textContent = lettersGuessed;
 
-            console.log(`Letters guessed: ${lettersGuessed}`);
-            console.log(`Guesses remaining: ${guessesLeft}`);
-
             // Check if guesses remain to continue
             if (guessesLeft === 0) {
-                // Will want to reveal the answer in the blanks - DON'T KNOW HOW YET
-                alert('Sorry, you lost! Try again.');
+                // Reveal answer somewhere using TextContent
+                // Could add removing puzzle from possible list so it's not reused
+                // Start a new game
                 startGame();
             }
         }
-
         //   If letter is in the puzzle, 
         else {
+            // For loop to see what position(s) the correct guess is in
+            for (i = 0; i < masterPuzzleLetters.length; i++) {
 
-            // Fill blank on screen with the letter - HAVEN'T FIGURED THIS OUT YET
+                if (userLetter === masterPuzzleLetters[i]) {
+
+                    // put userLetter in same index position in displayLetters array
+                    displayLetters.splice(i, 1, userLetter);
+                }
+            }
+
+            // Displays puzzle with correct guesses on screen
+            puzzleText.innerHTML = displayLetters;
 
             // Add letter to lettersGuessed array
             lettersGuessed.push(userLetter);
             lettersGuessedText.textContent = lettersGuessed;
 
-            console.log(`Letters guessed: ${lettersGuessed}`);
-
             // Remove letter guessed from puzzleLetters array
+            // This lets us know when a puzzle is solved
             for (let i = 0; i < puzzleLetters.length; i++) {
                 if (puzzleLetters[i] === userLetter) {
                     puzzleLetters.splice(i, 1);
-                    console.log(`Letters remaining to guess in puzzle: ${puzzleLetters}`);
                 }
+            }
+            console.log(puzzleLetters.length);
 
             // Check if puzzle was solved or if more guesses need to be made
-            // If puzzle solved:
             if (puzzleLetters.length === 0) {
-                wins ++;
+                // If puzzle solved:
+                wins++;
                 winsText.textContent = wins;
-                alert('You got it!');
+                // Could add removing puzzle from possible list so it's not reused
+
                 startGame();
             }
 
 
-            }
         }
     }
-    // Check to see if any more letters remain to be guessed
-
 }
-    
+
+
+
+
 
 
 
